6c45c8b681877fd0a96cddc0d243f4254c159e6f
SelectStarTop100 <test@test.com>
2022-11-26 14:53:57 -0500

    fix doc class speciality

--- HospitalApp/Doctor.cs
+++ HospitalApp/Doctor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,9 @@ namespace HospitalApp
 {
     internal class Doctor : Staff, IPaging
     {
-        public Doctor(string FirstName, string LastName, int EmpID) : base(FirstName, LastName, EmpID)
+        public Doctor(string FirstName, string LastName, int EmpID, string Speciality) : base(FirstName, LastName, EmpID)
         {
+            this.Speciality = Speciality;
         }
 
         public string Speciality { get; set; }
