aa2f0e709a306a6c3a34d0851900758f7c580d86
SelectStarTop100 <test@test.com>
2022-11-26 15:58:41 -0500

    started on while loop - fixed exit msg

--- HospitalApp/Program.cs
+++ HospitalApp/Program.cs
@@ -4,11 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Doctor doctor = new Doctor("Joe", "Smith", 99, "Lung");
-            
-                     
-            Console.WriteLine(doctor.GetDetails());
-            doctor.Page();
+            //Doctor doctor = new Doctor("Joe", "Smith", 99, "Lung");        
+            //Console.WriteLine(doctor.GetDetails());
+            //doctor.Page();
+            while (true)
+            {
+                Console.WriteLine("Enter any key to Continue. Type Exit to quit application");
+                string text = Console.ReadLine();
+                Console.Clear();
+                if (text == "Exit" | text == "exit")
+
+                {
+                    break;
+                }
+                else
+                {
+                    Doctor doctor = new Doctor("Joe", "Smith", 99, "Lung");
+                    Console.WriteLine(doctor.GetDetails());
+                    doctor.Page();
+                    //Console.WriteLine("Enter FirstName");
+                    //string InputFirstName = Console.ReadLine();
+                    //Console.Clear();
+                    //Console.WriteLine("Enter LastName");
+                    //string InputLastName = Console.ReadLine();
+                    //Console.Clear(); }
+
+                }
+
+            }
         }
     }
 }
