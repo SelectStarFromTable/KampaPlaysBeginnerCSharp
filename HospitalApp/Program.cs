f65692dc5b7f79e92647da457d8022257ce361c3
SelectStarTop100 <test@test.com>
2022-11-30 20:57:59 -0500

    create input doc class

--- HospitalApp/Program.cs
+++ HospitalApp/Program.cs
@@ -18,23 +18,43 @@
                 }
                 else
                 {
-                    Console.WriteLine("Enter Doctor FirstName");
-                    string? InputFirstName = Console.ReadLine();
-                    Console.Clear();
-                    Console.WriteLine("Enter Doctor LastName");
-                    string? InputLastName = Console.ReadLine();
-                    Console.WriteLine("Enter Doctor EmpID");
-                    int? InputEmpID = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter Doctor Speciality");
-                    string? InputSpeciality = Console.ReadLine();
-                    Doctors.Add(new Doctor(InputFirstName, InputLastName, (int)InputEmpID, InputSpeciality));
-                    Doctors.ForEach(doctor => { Console.WriteLine(doctor.GetDetails()); });
+
+                    InputDoctor();
+                    //Console.WriteLine("Enter Doctor FirstName");
+                    //string? InputFirstName = Console.ReadLine();
+                    //Console.Clear();
+                    //Console.WriteLine("Enter Doctor LastName");
+                    //string? InputLastName = Console.ReadLine();
+                    //Console.WriteLine("Enter Doctor EmpID");
+                    //int? InputEmpID = int.Parse(Console.ReadLine());
+                    //Console.WriteLine("Enter Doctor Speciality");
+                    //string? InputSpeciality = Console.ReadLine();
+                    //Doctors.Add(new Doctor(InputFirstName, InputLastName, (int)InputEmpID, InputSpeciality));
+                    //Doctors.ForEach(doctor => { Console.WriteLine(doctor.GetDetails()); });
                     //Console.WriteLine(NewDoctor.GetFullName());
                     //Console.Clear(); }
 
                 }
 
             }
+
+            void InputDoctor()
+            {
+                Console.WriteLine("Enter Doctor FirstName");
+                string? InputFirstName = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Enter Doctor LastName");
+                string? InputLastName = Console.ReadLine();
+                Console.WriteLine("Enter Doctor EmpID");
+                int? InputEmpID = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Doctor Speciality");
+                string? InputSpeciality = Console.ReadLine();
+                Doctors.Add(new Doctor(InputFirstName, InputLastName, (int)InputEmpID, InputSpeciality));
+                Doctors.ForEach(doctor => { Console.WriteLine(doctor.GetDetails()); });
+            };
+        
+        
+        
         }
     }
 }
