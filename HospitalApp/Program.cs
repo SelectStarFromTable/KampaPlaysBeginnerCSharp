7cf0f975a9b5f30a2dc67be10dd85a2b62a85c7c
SelectStarTop100 <test@test.com>
2022-11-30 21:00:46 -0500

    removed extra code from else statement

--- HospitalApp/Program.cs
+++ HospitalApp/Program.cs
@@ -19,20 +19,7 @@
                 else
                 {
 
-                    InputDoctor();
-                    //Console.WriteLine("Enter Doctor FirstName");
-                    //string? InputFirstName = Console.ReadLine();
-                    //Console.Clear();
-                    //Console.WriteLine("Enter Doctor LastName");
-                    //string? InputLastName = Console.ReadLine();
-                    //Console.WriteLine("Enter Doctor EmpID");
-                    //int? InputEmpID = int.Parse(Console.ReadLine());
-                    //Console.WriteLine("Enter Doctor Speciality");
-                    //string? InputSpeciality = Console.ReadLine();
-                    //Doctors.Add(new Doctor(InputFirstName, InputLastName, (int)InputEmpID, InputSpeciality));
-                    //Doctors.ForEach(doctor => { Console.WriteLine(doctor.GetDetails()); });
-                    //Console.WriteLine(NewDoctor.GetFullName());
-                    //Console.Clear(); }
+                    InputDoctor();  
 
                 }
 
